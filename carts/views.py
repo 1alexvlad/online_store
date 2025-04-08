31e11312e6a54b28010aa0f8c7bef94acad453f9
Vladislav Alexandrov <texr44760@gmail.com>
2025-04-08 22:01:29 +0300

    обновления количества товаров в корзине

--- carts/views.py
+++ carts/views.py
@@ -1,29 +1,75 @@
-from django.shortcuts import redirect, render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
 
 from product.models import Product
 from carts.models import Cart
 
 
-def cart_add(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+class CartAddView(View):
+    def post(self, request, product_slug):
+        
+        product = get_object_or_404(Product, slug=product_slug)
+        cart_item, created = Cart.objects.get_or_create(
+            user=request.user,
+            product=product,
+            defaults={'quantity': 1}
+        )
+        
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save()
+        
+        carts = Cart.objects.filter(user=request.user)
+        
+        return JsonResponse({
+            'success': True,
+            'total_quantity': carts.total_quantity(),
+            'item_quantity': cart_item.quantity,
+            'product_name': product.name
+        })
 
-    if request.user.is_authenticated:
-        carts = Cart.objects.filter(user=request.user, product=product)
 
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
-        else:
-            Cart.objects.create(user=request.user, product=product, quantity=1)
-    
-    return redirect("product:index")
+class CartChangeView(View):
+    def post(self, request, product_slug):
+        
+        product = get_object_or_404(Product, slug=product_slug)
+        cart_item = get_object_or_404(Cart, user=request.user, product=product)
+        
+        action = request.POST.get('action')
+        
+        if action == 'decrease':
+            if cart_item.quantity > 1:
+                cart_item.quantity -= 1
+                cart_item.save()
+            else:
+                cart_item.delete()
+                return JsonResponse({
+                    'success': True,
+                    'item_quantity': 0,
+                    'item_price': 0,
+                    'total_quantity': Cart.objects.filter(user=request.user).total_quantity(),
+                    'total_price': Cart.objects.filter(user=request.user).total_price(),
+                })
+        
+        carts = Cart.objects.filter(user=request.user)
+        
+        return JsonResponse({
+            'success': True,
+            'item_quantity': cart_item.quantity,
+            'item_price': cart_item.products_price(),
+            'total_quantity': carts.total_quantity(),
+            'total_price': carts.total_price(),
+        })
 
-def cart_change(request, product_slug):
-    pass
-
-def cart_remove(request, cart_id):
-    cart = Cart.objects.get(id=cart_id)
-    cart.delete()
-    return redirect('product:index')
+class CartRemoveView(View):
+    def post(self, request, cart_id):
+        cart_item = get_object_or_404(Cart, id=cart_id, user=request.user)
+        cart_item.delete()
+        
+        carts = Cart.objects.filter(user=request.user)
+        return JsonResponse({
+            'success': True,
+            'total_quantity': carts.total_quantity(),
+            'total_price': carts.total_price(),
+        })
